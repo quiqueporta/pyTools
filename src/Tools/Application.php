60c038883f134eadf5a345d88e8e230f8c1386bb
Quique Porta <quiqueporta@gmail.com>
2013-04-12 14:45:05 +0200

    Incrementada la version

--- src/Tools/Application.php
+++ src/Tools/Application.php
@@ -8,7 +8,7 @@ class Application extends BaseApplication
 {
     public function __construct()
     {
-        parent::__construct('PyndooTools', '0.5');
+        parent::__construct('PyndooTools', '0.6');
         $this->add(new \Apache\AddCommand());
         $this->add(new \Apache\DeleteCommand());
         $this->add(new \Apache\InfoCommand());
